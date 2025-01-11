f55813735ed3b6abf3c885dc4f1806f137cbc997
naymurweb <naymurweb@gmail.com>
2025-01-11 20:29:19 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
